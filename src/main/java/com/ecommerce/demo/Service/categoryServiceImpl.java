d80c67dd17b55e92116062baec91484c510aa2c7
Antara <antara.chakrabarti@lowes.com>
2024-09-30 00:23:03 +0530

    Managing IDs

--- src/main/java/com/ecommerce/demo/Service/categoryServiceImpl.java
+++ src/main/java/com/ecommerce/demo/Service/categoryServiceImpl.java
@@ -10,6 +10,7 @@ import java.util.List;
 public class categoryServiceImpl implements categoryService{
     //datatype of categories is categoryModel which is defined in categoryModel.java
     private List<categoryModel> categories = new ArrayList<>();
+    private Long nextId = 1L;//created this variable to manage ids
 
     @Override
     public List<categoryModel> getAllCategories() {
@@ -17,6 +18,7 @@ public class categoryServiceImpl implements categoryService{
     }
     @Override
     public void createCategory( categoryModel category){
+        category.setCategoryId(nextId++);//automatically increase the id when a new category will be added
         categories.add(category);
     }
 }
